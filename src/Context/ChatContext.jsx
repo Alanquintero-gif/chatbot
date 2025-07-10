e27bcb695adefb1991db71b5ec7aeb46e7913010
Alanquintero-gif <alangabrielquintero@gmail.com>
2025-07-10 14:54:15 -0300

    lastversion

--- src/Context/ChatContext.jsx
+++ src/Context/ChatContext.jsx
@@ -16,11 +16,13 @@ export function ChatProvider({ children }) {
     ],
     "Alfonsina Storni": [{ emisor: 'USUARIO', hora: '13:22', id: 5, texto: 'Hombre pequeñito...', status: 'visto' }
     ],
-    "Roberto Arlt": [      { emisor: 'USUARIO', hora: '18:47', id:6, texto: 'El futuro es nuestro por prepotencia de trabajo.', status: 'visto' }
+    "Roberto Arlt": [     { emisor: 'YO', hora: '14: 20', id: 1, texto: 'Roberto, qué futuro nos depara? ', status: 'visto' } 
+      ,{ emisor: 'USUARIO', hora: '18:47', id:6, texto: 'El futuro es nuestro por prepotencia de trabajo.', status: 'visto' }
     ],
     "Alejandra Pizarnik": [      { emisor: 'USUARIO', hora: '16:01', id:7, texto: 'Yo moriría por vos. Vos, ¿vivirías por mí?', status: 'visto' }
     ],
-    "Rodolfo Walsh": [ { emisor: 'USUARIO', hora: '12:33', id:8, texto: 'Sólo un débil mental puede no desear la paz.', status: 'visto' }],
+    "Rodolfo Walsh": [ { emisor: 'YO', hora: '10:30', id: 1, texto: 'Rodolfo, te enteraste de las nuevas guerras que hay en el mundo? ', status: 'visto' }
+      ,{ emisor: 'USUARIO', hora: '12:33', id:8, texto: 'Sólo un débil mental puede no desear la paz.', status: 'visto' }],
     "José Hernández": [ { emisor: 'USUARIO', hora: '11:11', id: 9, texto: 'Los hermanos sean unidos...', status: 'visto' }],
     "Sarmiento": [ { emisor: 'YO', hora: '14:18', id: 1, texto: 'Jorge, estoy desencantado con la escritura. Siempre soñé con estar en tu posición, pero, por alguna extraña razón, siento que nos estamos alejando. ', status: 'visto' },
       { emisor: 'USUARIO', hora: '19:45', id: 10, texto: 'Todos los problemas son problemas de educación.', status: 'visto' }],
